abc0c70621abe421581c9dbd089f05d321a43fa6
huangsongbo <2582737667@qq.com>
2017-03-26 01:03:40 +0800

    no message

--- src/main/java/com/amu/controller/StoreController.java
+++ src/main/java/com/amu/controller/StoreController.java
@@ -1,8 +1,28 @@
 package com.amu.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.amu.domain.Store;
+import com.amu.service.StoreService;
 
 @Controller
+@RequestMapping("/jsp/store")
 public class StoreController {
 
+	@Autowired
+	private StoreService storeService;
+	
+	@RequestMapping("/jspList.htm")
+	public Object jspList(HttpServletRequest request){
+		List<Store> storeList = storeService.findAll();
+		request.setAttribute("list", storeList);
+		return "/jsp/store/store_list.jsp";
+	}
+	
 }
--- src/main/java/com/amu/service/StoreService.java
+++ src/main/java/com/amu/service/StoreService.java
@@ -1,10 +1,23 @@
 package com.amu.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.amu.dao.StoreDao;
+import com.amu.domain.Store;
 
 @Service
 @Transactional
 public class StoreService {
+
+	@Autowired
+	private StoreDao storeDao;
+	
+	public List<Store> findAll() {
+		return storeDao.findAll();
+	}
 
 }
